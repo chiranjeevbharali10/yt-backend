ae4172c8722e742e2dcaf4202a69e952b5eb44a2
chiranjeevbharali10 <chiranjeevbharali12@gmail.com>
2025-12-20 12:55:08 +0000

    added a common aync and done middleware

--- src/app.js
+++ src/app.js
@@ -8,12 +8,15 @@ const app = express()
 
 app.use(cors({
 
-  origin : process.env.CORS_ORIGIN
+  origin : process.env.CORS_ORIGIN,
+  credentials : true
 }))
 
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true , limit: "16kb"}))
 app.use(express.static("public"))
+//basically for server to read your cookeies that is sent by the browser basically  
 app.use(cookieParser())
+
 export {app}
